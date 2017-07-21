17ba7752aacf01c8dcfcdfe1574fb6e8f5cac7ee
Alex Bogdanovski <alex@erudika.com>
2017-07-21 19:17:21 +0300

    tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Run mvn release:perform first!"
 read -e -p "Tag: " ver
 read -e -p "Last tag: " lastver
 git add -A && git commit -m "Release v$ver." && git push origin master
@@ -7,7 +8,7 @@ echo "" >> changelog.txt
 git log $lastver..HEAD --oneline >> changelog.txt
 echo "" >> changelog.txt
 echo "" >> changelog.txt
-echo "[Download JAR](https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-search-elasticsearch/${ver}/para-search-elasticsearch-${ver}-shaded.jar)" >> changelog.txt
+echo "### :package: [Download JAR](https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-search-elasticsearch/${ver}/para-search-elasticsearch-${ver}-shaded.jar)" >> changelog.txt
 hub release create -F changelog.txt $ver
 cat changelog.txt
 rm changelog.txt
