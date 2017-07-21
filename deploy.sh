88565eeb96611242c2b1f31fedda1d4d36579aa6
Alex Bogdanovski <alex@erudika.com>
2017-07-21 19:05:23 +0300

    Release v1.25.0.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 read -e -p "Tag: " ver
-git add -A && git commit -m "Release v$ver."
-git tag "$ver"
-git push origin master && git push --tags
-
-echo "v$ver \n" > changelog.txt
-git log $ver..HEAD --oneline >> changelog.txt
-hub release create -F changelog.txt -a target/para-search-elasticsearch-*.jar 
+read -e -p "Last tag: " lastver
+git add -A && git commit -m "Release v$ver." && git push origin master
+echo "v$ver" > changelog.txt
+echo "" >> changelog.txt
+git log $lastver..HEAD --oneline >> changelog.txt
+echo "" >> changelog.txt
+echo "" >> changelog.txt
+echo "[Download JAR](https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-search-elasticsearch/${ver}/para-search-elasticsearch-${ver}-shaded.jar)" >> changelog.txt
+hub release create -F changelog.txt
+cat changelog.txt
+rm changelog.txt
