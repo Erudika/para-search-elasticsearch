0611c580548540b1f252a89002386f554dd3bb67
Alex Bogdanovski <alex@erudika.com>
2017-07-23 17:43:16 +0300

    Release v1.25.1.

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,15 @@ read -e -p "Version tag: " ver
 read -e -p "Last version tag: " lastver
 read -e -p "New dev version: " devver
 
-git add -A && git commit -m "Release v$ver." && git push origin master
-
+git add -A && git commit -m "Release v$ver." && git push origin master && \
 mvn --batch-mode -Dtag=${ver} release:prepare -DreleaseVersion=${ver} -DdevelopmentVersion=${devver}-SNAPSHOT && \
-mvn release:perform
-
-echo "Maven release done, publishing release on GitHub..,"
-echo "v$ver" > changelog.txt
-echo "" >> changelog.txt
-git log $lastver..HEAD --oneline >> changelog.txt
-echo "" >> changelog.txt
-echo "" >> changelog.txt
-echo "### :package: [Download JAR](https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-search-elasticsearch/${ver}/para-search-elasticsearch-${ver}-shaded.jar)" >> changelog.txt
-hub release create -F changelog.txt $ver
-cat changelog.txt
+mvn release:perform && \
+echo "Maven release done, publishing release on GitHub..," && \
+echo "v$ver" > changelog.txt && \
+echo "" >> changelog.txt && \
+git log $lastver..HEAD --oneline >> changelog.txt && \
+echo "" >> changelog.txt && \
+echo "" >> changelog.txt && \
+echo "### :package: [Download JAR](https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-search-elasticsearch/${ver}/para-search-elasticsearch-${ver}-shaded.jar)" >> changelog.txt && \
+hub release create -F changelog.txt $ver && \
 rm changelog.txt
